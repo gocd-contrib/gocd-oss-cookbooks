684d170c07161c60ede07337c01a83a119f3d3b9
Ketan Padegaonkar <ketanpadegaonkar@gmail.com>
2016-09-28 00:36:13 +0530

    Ensure that fpm is installed

---
+++ custom-cookbooks/fpm/metadata.rb
@@ -0,0 +1,8 @@
+name             'fpm'
+maintainer       'ThoughtWorks, Inc.'
+maintainer_email 'support@snap-ci.com'
+license          'MIT'
+description      'Installs/Configures fpm'
+version          '0.1.0'
+
+depends 'ruby'
---
+++ custom-cookbooks/fpm/recipes/default.rb
@@ -0,0 +1,3 @@
+include_recipe 'ruby'
+
+execute '/bin/bash -lc "gem install fpm --no-ri --no-rdoc"'
