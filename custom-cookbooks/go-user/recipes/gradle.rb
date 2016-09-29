59fba8b85d4bf9d3370fbddc17829879a57a2e6d
Ketan Padegaonkar <ketanpadegaonkar@gmail.com>
2016-09-29 13:42:30 +0530

    Run gradle build before setting up the init script

--- custom-cookbooks/go-user/recipes/gradle.rb
+++ custom-cookbooks/go-user/recipes/gradle.rb
@@ -1,18 +1,6 @@
 # ensure that gradle is installed in `/go/.gradle` via gradle-wrapper
 include_recipe 'java'
 
-directory '/go/.gradle' do
-  owner 'go'
-  group 'go'
-  mode  '0755'
-end
-
-cookbook_file '/go/.gradle/init.gradle' do
-  owner 'go'
-  group 'go'
-  mode  '0600'
-end
-
 bash 'initialize gocd build' do
   user  'go'
   group 'go'
@@ -25,8 +13,20 @@ bash 'initialize gocd build' do
     cd /tmp
     git clone https://github.com/gocd/gocd --depth 1 --quiet
     cd gocd
-    ./gradlew clean prepare --max-workers 4 > /dev/null 2>&1
+    ./gradlew clean prepare --max-workers 4
     cd /
     rm -rf /tmp/gocd
   "
+end
+
+directory '/go/.gradle' do
+  owner 'go'
+  group 'go'
+  mode  '0755'
+end
+
+cookbook_file '/go/.gradle/init.gradle' do
+  owner 'go'
+  group 'go'
+  mode  '0600'
 end
