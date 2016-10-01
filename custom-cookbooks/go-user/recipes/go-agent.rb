c6acb7012862e910996a8cde0bc7f68d753a81ec
Ketan Padegaonkar <ketanpadegaonkar@gmail.com>
2016-10-01 13:26:31 +0530

    Use 0.4 agent

--- custom-cookbooks/go-user/recipes/go-agent.rb
+++ custom-cookbooks/go-user/recipes/go-agent.rb
@@ -2,5 +2,5 @@ remote_file '/go/go-agent' do
   owner 'root'
   group 'root'
   mode '0755'
-  source 'https://github.com/ketan/gocd-golang-bootstrapper/releases/download/0.3/go-bootstrapper-0.3.linux.amd64'
+  source 'https://github.com/ketan/gocd-golang-bootstrapper/releases/download/0.4/go-bootstrapper-0.4.linux.amd64'
 end
