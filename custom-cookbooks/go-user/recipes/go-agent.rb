4e5b708454c555b34d5932ad3d5023de2f450590
Ketan Padegaonkar <ketanpadegaonkar@gmail.com>
2016-10-03 09:44:59 +0530

    Bump to 0.6

--- custom-cookbooks/go-user/recipes/go-agent.rb
+++ custom-cookbooks/go-user/recipes/go-agent.rb
@@ -2,5 +2,5 @@ remote_file '/go/go-agent' do
   owner 'root'
   group 'root'
   mode '0755'
-  source 'https://github.com/ketan/gocd-golang-bootstrapper/releases/download/0.5/go-bootstrapper-0.5.linux.amd64'
+  source 'https://github.com/ketan/gocd-golang-bootstrapper/releases/download/0.6/go-bootstrapper-0.6.linux.amd64'
 end
