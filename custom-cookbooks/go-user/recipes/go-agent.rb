63896a58a5ea0140cb13b50501eba5af9a996417
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2017-05-09 17:42:33 +0530

    Use newer bootstrapper

--- custom-cookbooks/go-user/recipes/go-agent.rb
+++ custom-cookbooks/go-user/recipes/go-agent.rb
@@ -2,7 +2,7 @@ remote_file '/go/go-agent' do
   owner 'root'
   group 'root'
   mode '0755'
-  source 'https://github.com/ketan/gocd-golang-bootstrapper/releases/download/0.8/go-bootstrapper-0.8.linux.amd64'
+  source 'https://github.com/ketan/gocd-golang-bootstrapper/releases/download/1.0/go-bootstrapper-1.0.linux.amd64'
 end
 
 cookbook_file '/bootstrap.sh' do
