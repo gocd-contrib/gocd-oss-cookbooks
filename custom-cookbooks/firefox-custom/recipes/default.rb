949b498a1db3bafbea6bed0b9220a261d58a22a0
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2017-11-10 16:28:58 +0530

    Tweaks for centos 7

--- custom-cookbooks/firefox-custom/recipes/default.rb
+++ custom-cookbooks/firefox-custom/recipes/default.rb
@@ -2,11 +2,17 @@
 
 pkgs = ['firefox']
 
-pkgs += %w(gnome-themes hicolor-icon-theme xdotool)
+if platform_family?('rhel', 'suse', 'amazon')
+  if node['platform_version'].to_i < 7
+    pkgs += %w(gnome-themes xdotool nspluginwrapper)
+  end
+end
+
+
+pkgs += %w(hicolor-icon-theme)
 
 pkgs += %w(dbus dbus-x11 xauth liberation-sans-fonts liberation-serif-fonts liberation-mono-fonts mesa-dri-drivers)
-# plugins
-pkgs += %w(nspluginwrapper)
+
 # x11 stuff
 pkgs += %w(xorg-x11-fonts-100dpi xorg-x11-fonts-75dpi xorg-x11-fonts-Type1 xorg-x11-fonts-cyrillic urw-fonts)
 
