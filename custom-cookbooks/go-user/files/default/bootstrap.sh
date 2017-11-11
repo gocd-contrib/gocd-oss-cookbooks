b6a8774625a6ef26e209dedc7a271fa726dbc5aa
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2017-11-11 08:28:07 +0530

    Fix startup script

--- custom-cookbooks/go-user/files/default/bootstrap.sh
+++ custom-cookbooks/go-user/files/default/bootstrap.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+yell() { echo "$0: $*" >&2; }
+die() { yell "$*"; exit 111; }
+try() { echo "$ $@" 1>&2; "$@" || die "cannot $*"; }
+
 export DISPLAY=:3
 
-sudo /etc/init.d/messagebus start && \
-  vncserver :3 -geometry '1280x960' -depth 16 && \
-  exec /go/go-agent
+OS_VERSION="$(lsb_release -a | grep Release | cut -f2 | cut -f1 -d.)"
+
+if [ OS_VERSION == "6" ]; then
+  try sudo /etc/init.d/messagebus start
+fi
+
+try vncserver :3 -geometry '1280x960' -depth 16
+try exec /go/go-agent
