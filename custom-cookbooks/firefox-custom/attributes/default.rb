a4246fbe610fc5e6d11603d76a2a46215bbf5bbb
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2017-11-13 12:57:23 +0530

    Install latest firefox as well

--- custom-cookbooks/firefox-custom/attributes/default.rb
+++ custom-cookbooks/firefox-custom/attributes/default.rb
@@ -1,2 +1,2 @@
-default['firefox-custom']['versions']     = %w(24.5.0esr 45.4.0esr)
+default['firefox-custom']['versions']     = %w(24.5.0esr 45.4.0esr latest)
 default['firefox-custom']['install_dir'] = '/opt/local/firefox'
