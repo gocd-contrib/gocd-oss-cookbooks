5c357c640092cfa58d8474b72c5ced8d55595439
rajiesh <rajiesh.narayanan@gmail.com>
2017-11-22 13:28:22 +0530

    Changes to build docker images with different versions of postgresql
    Image to be used for running regression test on each version of postgresql

--- custom-cookbooks/postgresql-custom/metadata.rb
+++ custom-cookbooks/postgresql-custom/metadata.rb
@@ -3,7 +3,7 @@ maintainer       'ThoughtWorks, Inc.'
 maintainer_email 'go-cd-dev@googlegroups.com'
 license          'MIT'
 description      'Installs and configures postgresql'
-version          '0.1.1'
+version          '0.1.2'
 
 supports 'centos'
 depends  'postgresql'
--- custom-cookbooks/postgresql-custom/recipes/default.rb
+++ custom-cookbooks/postgresql-custom/recipes/default.rb
@@ -1,13 +1,25 @@
 include_recipe 'postgresql::yum_pgdg_postgresql'
 
-package %w(postgresql96 postgresql96-devel postgresql96-contrib postgresql96-server)
+case node['postgresql']['version']
+when "9.2"
+  package %w(postgresql92 postgresql92-devel postgresql92-server postgresql92-contrib)
+when "9.3"
+  package %w(postgresql93 postgresql93-devel postgresql93-server postgresql93-contrib)
+when "9.4"
+  package %w(postgresql94 postgresql94-devel postgresql94-server postgresql94-contrib)
+when "9.5"
+  package %w(postgresql95 postgresql95-devel postgresql95-server postgresql95-contrib)
+when "9.6"
+  package %w(postgresql96 postgresql96-devel postgresql96-server postgresql96-contrib)
+end
 
 directory '/var/run/postgresql' do
   owner  'go'
   group  'go'
 end
 
-cookbook_file '/etc/profile.d/postgresql-custom.sh' do
+file '/etc/profile.d/postgresql-custom.sh' do
+  content "export PATH=$PATH:/usr/pgsql-#{node['postgresql']['version']}/bin"
   owner  'root'
   group  'root'
   mode   '0644'
