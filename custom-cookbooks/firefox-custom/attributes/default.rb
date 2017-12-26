8c4408854591c33c6dbe54a66929845c88d5787d
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2017-12-26 17:02:52 +0530

    Add FF 56.0.2 to the mix

--- custom-cookbooks/firefox-custom/attributes/default.rb
+++ custom-cookbooks/firefox-custom/attributes/default.rb
@@ -1,2 +1,2 @@
-default['firefox-custom']['versions']     = %w(24.5.0esr 45.4.0esr latest)
+default['firefox-custom']['versions']     = %w(24.5.0esr 45.4.0esr 56.0.2 latest)
 default['firefox-custom']['install_dir'] = '/opt/local/firefox'
