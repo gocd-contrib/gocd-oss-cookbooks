3e60dab1f94b79138653bfc129763aea3c4869c6
Kritika Singh <kritika.singh1994@gmail.com>
2019-09-23 10:57:00 +0530

    Updated the default java version used to Java 11 as it has LTS Also updated the with-java for the same

--- provision/with-java.ps1
+++ provision/with-java.ps1
@@ -4,14 +4,14 @@ if (!${env:JAVA_VERSION}) {
     $env:JAVA_VERSION=11
 }
 
-if ("${env:JAVA_VERSION}" -eq "1.8") {
-    jabba use "1.8"
-}
-elseif ("${env:JAVA_VERSION}" -eq "11") {
+if ("${env:JAVA_VERSION}" -eq "11") {
     jabba use "openjdk@1.11"
 }
 elseif ("${env:JAVA_VERSION}" -eq "12") {
     jabba use "openjdk@1.12"
+}
+elseif ("${env:JAVA_VERSION}" -eq "13") {
+    jabba use "openjdk@1.13"
 }
 
 if ($args.Length -eq 0) {
