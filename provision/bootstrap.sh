75d821bf1a0c8fef93f13cd9a21c4b2a79a38f7c
Marques Lee <marques.lee@thoughtworks.com>
2020-07-04 21:03:32 -0700

    Fix bootstrap.sh for CentOS 8

--- provision/bootstrap.sh
+++ provision/bootstrap.sh
@@ -4,9 +4,9 @@ yell() { echo "$0: $*" >&2; }
 die() { yell "$*"; exit 111; }
 try() { echo "$ $@" 1>&2; "$@" || die "cannot $*"; }
 
-CENTOS_MAJOR_VERSION=$(rpm -qa \*-release | grep -Ei "oracle|redhat|centos" | cut -d"-" -f3)
+CENTOS_MAJOR_VERSION=$(rpm -qa \*-release | grep -Ei "oracle|redhat|centos" | cut -d"-" -f3 | cut -d"." -f1)
 
-if [ "$CENTOS_MAJOR_VERSION" == "7" ]; then
+if [ "$CENTOS_MAJOR_VERSION" -ge "7" ]; then
   # since we do not install X and FF on centos 6
   export DISPLAY=:3
   try Xvfb :3 -screen 0 1280x960x16 &
