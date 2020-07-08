8c8726dc3f4dd5f7839945274ee90fde79d79634
Marques Lee <marques.lee@thoughtworks.com>
2020-07-08 16:46:54 -0700

    Install firefox and chromium-based microsoft edge on windows

--- provision/install-packages.ps1
+++ provision/install-packages.ps1
@@ -53,6 +53,7 @@ choco install --no-progress -y ruby --version="${RUBY_VERSION}"
 choco install --no-progress -y nant --version="${NANT_VERSION}"
 choco install --no-progress -y ant -i --version="${ANT_VERSION}"
 choco install --no-progress -y hg yarn svn git gpg4win-vanilla windows-sdk-8.1 awscli
+choco install --no-progress -y firefox microsoft-edge
 
 RefreshEnv
 
