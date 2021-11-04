e434e62bf8d977c850862cda4612a4f4c2b8662d
Chad Wilson <chadw@thoughtworks.com>
2021-11-04 16:31:17 +0800

    Attempt to fix the bootstrapping of caches for server
    Currently Gradle is dying with Yarn timeouts while doing initial setup, probably due to slow I/O on GitHub Actions.
    Due to other issues with the scripting, it doesn't actually stop on an error and just keeps going. Oops.
    
    The timeouts seems a common problem with Yarn 1, only addressed with Yarn 2 :( https://github.com/yarnpkg/yarn/issues/8242

--- provision/init-gradle-cache.ps1
+++ provision/init-gradle-cache.ps1
@@ -6,10 +6,12 @@ Write-Host "Builing gocd to install gradle and build gradle cache"
 
 git clone https://github.com/gocd/gocd --depth 1 C:\\gocd --quiet
 cd C:\\gocd
+yarn.cmd config set network-timeout 300000
 ./gradlew.bat prepare --no-build-cache --quiet
-timeout 5
+yarn.cmd config delete network-timeout
+Start-Sleep -Seconds 5
 ./gradlew.bat clean --quiet
-timeout 5
+Start-Sleep -Seconds 5
 tasklist
 taskkill /F /IM java.exe
 git clean -ffddx
