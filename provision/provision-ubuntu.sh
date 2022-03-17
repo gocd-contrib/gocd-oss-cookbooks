0a3cdffb5362d802282f102362848c65bbb327c6
Chad Wilson <chadw@thoughtworks.com>
2022-03-17 19:43:04 +0800

    Bump Ruby version to 2.7.5

--- provision/provision-ubuntu.sh
+++ provision/provision-ubuntu.sh
@@ -17,7 +17,7 @@ function provision() {
   step install_scm_tools
 
   step install_rbenv
-  step install_global_ruby "2.7.2"
+  step install_global_ruby "2.7.5"
 
   step install_nodenv
   step install_global_node "16.14.1"
