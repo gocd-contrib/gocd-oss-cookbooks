5802221dccd1c8c6ec5d8c04903d33afdd321904
Chad Wilson <chadw@thoughtworks.com>
2022-10-02 16:36:26 +0800

    Try to switch to quicker mechanism of cleaning up the temporary GoCD clone folder

--- provision/init-gradle-cache.ps1
+++ provision/init-gradle-cache.ps1
@@ -2,20 +2,18 @@
 
 $ErrorActionPreference = "Stop"
 
-Write-Host "Builing gocd to install gradle and build gradle cache"
-
+Write-Host "Builing gocd to install gradle and build gradle cache..."
 git clone https://github.com/gocd/gocd --depth 1 C:\\gocd --quiet
 cd C:\\gocd
 yarn.cmd config set network-timeout 300000
 ./gradlew.bat prepare --no-build-cache --quiet
 yarn.cmd config delete network-timeout
-Start-Sleep -Seconds 5
-./gradlew.bat clean --quiet
-Start-Sleep -Seconds 5
-tasklist
-taskkill /F /IM java.exe
-git clean -ffddx
-tasklist
-cd \
-Remove-Item -Path C:\\gocd -Force -Recurse | Out-Null
+
+Write-Host "Stopping Gradle daemons..."
+./gradlew.bat --stop
+
+Write-Host "Cleaning up..."
+cd c:\\
+cmd /c "rmdir /s /q C:\\gocd"
+
 New-Item C:\\go -ItemType Directory | Out-Null
