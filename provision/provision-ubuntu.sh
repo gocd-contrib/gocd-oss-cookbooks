42627aa8321fe36997e0b98435ca0082ddc173ed
Chad Wilson <chadw@thoughtworks.com>
2022-10-28 19:27:30 +0800

    Upgrade NodeJS from 16.18.0 to 18.12.0
    NodeJS 18 is now LTS.

--- provision/provision-ubuntu.sh
+++ provision/provision-ubuntu.sh
@@ -25,7 +25,7 @@ function provision() {
   step install_global_asdf "ruby" "3.1.2"
   step install_global_ruby_default_gems
 
-  step install_global_asdf "nodejs" "16.18.0"
+  step install_global_asdf "nodejs" "18.12.0"
   step install_yarn
 
   step install_python
