02fa77e7e4b19fad9158a735bf69b7f8b3c97983
Chad Wilson <chadw@thoughtworks.com>
2023-01-24 01:31:04 +0800

    Bump global JDK version to 17.0.6

--- provision/provision-centos.sh
+++ provision/provision-centos.sh
@@ -47,8 +47,8 @@ function provision() {
 
   step install_asdf "v0.11.1" "java" "ruby" "nodejs"
 
-  step install_global_asdf "java" "temurin-17.0.5+8"
-  step install_multi_asdf "java" "temurin-17.0.5+8"
+  step install_global_asdf "java" "temurin-17.0.6+10"
+  step install_multi_asdf "java" "temurin-17.0.6+10"
 
   step install_global_asdf "ruby" "3.1.3"
   step install_global_ruby_default_gems
