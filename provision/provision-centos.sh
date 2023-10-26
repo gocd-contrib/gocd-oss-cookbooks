ccc134fc8d44c70318694ac872c5174db9f48812
Chad Wilson <chadw@thoughtworks.com>
2023-10-26 22:38:12 +0800

    Bump Gauge to 1.5.5

--- provision/provision-centos.sh
+++ provision/provision-centos.sh
@@ -58,7 +58,7 @@ function provision() {
   step install_maven "$MAVEN_VERSION"
   step install_ant "$ANT_VERSION"
 
-  step install_gauge "1.5.4"
+  step install_gauge "1.5.5"
   step install_installer_tools
   step install_awscli_mimetypes
   step install_awscli
