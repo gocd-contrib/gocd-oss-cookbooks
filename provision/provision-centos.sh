e390e37adbf57787d8a082162fa0a13a448cd071
Chad Wilson <chadw@thoughtworks.com>
2024-03-27 11:54:55 +0800

    Bump NodeJS to 20.12.0

--- provision/provision-centos.sh
+++ provision/provision-centos.sh
@@ -47,7 +47,7 @@ function provision() {
   step install_mise_tools \
     "java@temurin-17.0.10+7" \
     "ruby@3.3.0" \
-    "node@20.11.1"
+    "node@20.12.0"
   step install_ruby_default_gems
   step install_yarn
 
