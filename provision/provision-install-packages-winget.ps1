de5e936e11fcca56fec95db67cbc896f6744af44
Chad Wilson <chadw@thoughtworks.com>
2025-01-19 17:32:20 +0800

    Convert from usage of Chocolatey to Winget

--- provision/provision-install-packages-winget.ps1
+++ provision/provision-install-packages-winget.ps1
@@ -1,9 +1,10 @@
 $JAVA_VERSION='21.0.5.11'
 $NODEJS_VERSION='22.13.0'
-$RUBY_VERSION='3.4.1.1'
 
 $GOLANG_BOOTSTRAPPER_VERSION='2.23'
-$P4D_VERSION='24.1'
+$P4_VERSION='24.1'
+$ANT_VERSION='1.10.15'
+$NANT_VERSION='0.92'
 
 # Copy over configs
 New-Item "${env:USERPROFILE}\.gradle" -ItemType Directory | Out-Null
@@ -16,6 +17,10 @@ Copy-Item "$PSScriptroot\init.gradle"         "${env:USERPROFILE}\.gradle\init.g
 Copy-Item "$PSScriptroot\npmrc"               "${env:USERPROFILE}\.npmrc"
 Copy-Item "$PSScriptroot\settings.xml"        "${env:USERPROFILE}\.m2\settings.xml"
 
+function Winget-Install {
+    winget install --accept-source-agreements --accept-package-agreements --disable-interactivity @args
+}
+
 function PrefixToSystemAndCurrentPath {
     param (
         [Parameter(Mandatory=$true)]
@@ -26,49 +31,38 @@ function PrefixToSystemAndCurrentPath {
     [Environment]::SetEnvironmentVariable("Path", $newSystemPath, [EnvironmentVariableTarget]::Machine)
 }
 
-# install chocolatey
-$chocolateyUseWindowsCompression = 'true'
-$env:chocolateyUseWindowsCompression = 'true'
-$ErrorActionPreference = "Stop"
-$progressPreference = 'silentlyContinue'
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Force
-Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
-iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-
-$env:ChocolateyInstall = Convert-Path "$((Get-Command choco).path)\..\.."
-Import-Module "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-RefreshEnv
-
 # install packages
-choco install --no-progress -y nodejs --version="${NODEJS_VERSION}"
-RefreshEnv
+Winget-Install OpenJS.NodeJS.LTS --version="${NODEJS_VERSION}"
 corepack enable
 yarn --version
 
-choco install --no-progress -y temurin --version="${JAVA_VERSION}"
-choco install --no-progress -y git --params "/NoAutoCrlf"
-choco install --no-progress -y nant ant hg sliksvn gnupg awscli
-choco install --no-progress -y --ignore-checksums p4  # Ignore checksums due to package not using repeatable build links to downloads
-choco install --no-progress -y windows-sdk-11-version-22h2-all --install-arguments='/features OptionId.SigningTools /ceip off'
-choco install --no-progress -y --ignore-checksums googlechrome # Ignore checksums due to package not using repeatable build links to downloads
+Winget-Install EclipseAdoptium.Temurin.21.JDK --version="${JAVA_VERSION}"
 
-choco install --no-progress -y ruby --version="${RUBY_VERSION}"
-# Install MSYS2 and dev toolchain for compiling certain native Ruby extensions, introduced for google-protobuf 3.25.0+
-# Following pattern at https://community.chocolatey.org/packages/msys2#description
-$msysInstallDir = "C:\tools\msys64"
-choco install --no-progress -y msys2 --params "/NoUpdate /InstallDir:${msysInstallDir}"
-RefreshEnv
-PrefixToSystemAndCurrentPath("${msysInstallDir}\ucrt64\bin;${msysInstallDir}\usr\bin") # Manually add MSYS2 and tools to path to avoid having to do shell-specific "ridk enable" in builds.
+Winget-Install Git.Git Mercurial.Mercurial Slik.Subversion GnuPG.GnuPG Amazon.AWSCLI Google.Chrome
+git config --global core.autocrlf false
+
+Winget-Install Microsoft.WindowsSDK --override='/features OptionId.SigningTools /ceip off'
+
+Winget-Install RubyInstallerTeam.RubyWithDevKit.3.2
 ridk install 2 3 # Update packages and install development toolchain
 
-# Remove chocolatey from temp location
-Remove-Item C:\\Users\\ContainerAdministrator\\AppData\\Local\\Temp\\chocolatey -Force -Recurse | Out-Null
-
-# install p4d / helix-core-server
+# install p4 client and p4d / helix-core-server
 New-Item "${env:ProgramFiles}\\Perforce\\bin\\" -ItemType Directory | Out-Null
-Invoke-WebRequest https://cdist2.perforce.com/perforce/r$P4D_VERSION/bin.ntx64/p4d.exe -Outfile "${env:ProgramFiles}\\Perforce\\bin\\p4d.exe"
 PrefixToSystemAndCurrentPath("${env:ProgramFiles}\\Perforce\\bin")
+Invoke-WebRequest https://cdist2.perforce.com/perforce/r$P4_VERSION/bin.ntx64/p4.exe -Outfile "${env:ProgramFiles}\\Perforce\\bin\\p4.exe"
+Invoke-WebRequest https://cdist2.perforce.com/perforce/r$P4_VERSION/bin.ntx64/p4d.exe -Outfile "${env:ProgramFiles}\\Perforce\\bin\\p4d.exe"
+
+# Install ant
+Invoke-WebRequest https://dlcdn.apache.org//ant/binaries/apache-ant-${ANT_VERSION}-bin.zip -Outfile "${env:TEMP}\\ant.zip"
+Expand-Archive -Path "${env:TEMP}\\ant.zip" -DestinationPath "C:\\tools"
+PrefixToSystemAndCurrentPath("C:\\tools\\apache-ant-${ANT_VERSION}\\bin")
+Remove-Item "${env:TEMP}\\ant.zip" -Force
+
+# Install nant
+Invoke-WebRequest https://onboardcloud.dl.sourceforge.net/project/nant/nant/${NANT_VERSION}/nant-${NANT_VERSION}-bin.zip?viasf=1 -Outfile "${env:TEMP}\\nant.zip"
+Expand-Archive -Path "${env:TEMP}\\nant.zip" -DestinationPath "C:\\tools"
+PrefixToSystemAndCurrentPath("C:\\tools\\nant-${NANT_VERSION}\\bin")
+Remove-Item "${env:TEMP}\\nant.zip" -Force
 
 # install gocd bootstrapper
 Invoke-WebRequest https://github.com/gocd-contrib/gocd-golang-bootstrapper/releases/download/${GOLANG_BOOTSTRAPPER_VERSION}/go-bootstrapper-${GOLANG_BOOTSTRAPPER_VERSION}.windows.amd64.exe -Outfile C:\\go-agent.exe
