1e2f9aff08875031b331b4586ad0b9f5e558ae08
Chad Wilson <29788154+chadlwilson@users.noreply.github.com>
2025-08-29 12:18:51 +0800

    Bump node from 22.18.0 to 22.19.0

--- provision/provision-ubuntu.sh
+++ provision/provision-ubuntu.sh
@@ -20,7 +20,7 @@ function provision() {
   step install_mise_tools \
     "java@temurin-21.0.8+9.0.LTS" \
     "ruby@3.4.5" \
-    "node@22.18.0"
+    "node@22.19.0"
   step install_ruby_default_gems
   step install_yarn
 
